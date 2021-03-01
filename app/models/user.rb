8f32f5eb3fb35886d47b15d093d92ffe7e81674b
sogo7 <Sogohara7@icloud.com>
2021-03-01 23:14:16 +0900

    goodボタン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
+  has_many :posts
+  has_many :goods, dependent: :destroy
+  has_many :good_posts, through: :goods, source: :post
 end
