1643f6017b00b428a1360bd5043983ec017e11bf
sogo7 <Sogohara7@icloud.com>
2021-03-07 11:33:50 +0900

    4つのリンク作成！

--- app/controllers/my_ogiris_controller.rb
+++ app/controllers/my_ogiris_controller.rb
@@ -1,4 +1,5 @@
-class MyOgirisController < ApplicationController
+class MyOgirisController < PostsController
   def index
+    @posts = current_user.posts.all
   end
 end
--- app/controllers/new_arrivals_controller.rb
+++ app/controllers/new_arrivals_controller.rb
@@ -1,4 +1,5 @@
 class NewArrivalsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
   end
 end
