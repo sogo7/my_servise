216df3e0d8dc2528124803c7c7fb524859d2df93
sogo7 <Sogohara7@icloud.com>
2021-03-07 20:58:23 +0900

    nice機能編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,32 @@ Rails.application.routes.draw do
   get 'new_arrivals/index'
   get 'popular_ogiris/index'
   get 'my_ogiris/index'
+  
   get 'comments/create'
   get 'comments/destroy'
+
   resources :posts
-  resources :users
-  root  'tops#index'
-  resources :posts, except: [:index] do
-    resources :comments, only: [:create, :destroy]
-  end
-  resources :posts do
-    post 'add' => 'goods#create'
-    delete '/add' => 'goods#destroy'
-  end
-  resources :comments do
-    post 'add' => 'nices#create'
-    delete '/add' => 'nices#destroy'
-  end
+
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
+
+  resources :users, only: [:show]
+
+  root  'tops#index'
+
+  resources :posts, except: [:index] do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  resources :posts do
+    post 'add' => 'goods#create'
+    delete '/add' => 'goods#destroy'
+  end
+
+  resources :comments do
+    post 'add' => 'nices#create'
+    delete '/add' => 'nices#destroy'
+  end
 end
